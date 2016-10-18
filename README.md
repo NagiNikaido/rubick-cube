c04b1f08a399e746405af1bec330fbe5eb9cf7f4
naginikaido <1203794101@qq.com>
2016-10-18 17:10:33 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# rubick-cube
+
+------
+
+The project for Introduction to Computer Programming.
+
+## Data stucture
+
+
+
+
+|   | B |   |   |
+|:-:|:-:|:-:|:-:|
+| L | U | R | D |
+|   | F |   |   ||
+
+
+All the 6 six colours are R(ed), W(hite), O(range), B(lue), G(reen) and Y(ellow). Also you can use 0-6 to represent these colours.
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const char color[10]="RWOBGY";
+
+// Up --- 0
+// Back -- 1
+// Right -- 2
+// Front -- 3
+// Left -- 4
+// Down -- 5
+
+class RubickCube{
+private:
+	int faces[6][3][3];
+public:
+	
+	void input(istream & is){
+		//Back
+		for(int i=2;i>=0;i--)
+			for(int j=2;j>=0;j--)
+				is >> faces[1][i][j];
+		//Left Up Right Down
+		for(int i=0;i<3;i++){
+			for(int j=2;j>=0;j--) is >> faces[4][j][i];
+			for(int j=0;j<3;j++) is >> faces[0][i][j];
+			for(int j=0;j<3;j++) is >> faces[2][j][2-i];
+			for(int j=2;j>=0;j--) is >> faces[5][i][2-j];
+		}
+		//Front
+		for(int i=0;i<3;i++)
+			for(int j=0;j<3;j++)
+				is >> faces[3][i][j];
+	}
+	void dump(ostream & os){
+		//Back
+		for(int i=2;i>=0;i--){
+			os << ' ' << ' ' << ' ' << ' ' << ' ' << ' ';
+			for(int j=2;j>=0;j--) os << faces[1][i][j] << ' ';
+			os << endl;
+		}
+		//Left Up Right Down
+		for(int i=0;i<3;i++){
+			for(int j=2;j>=0;j--) os << faces[4][j][i] << ' ';
+			for(int j=0;j<3;j++) os << faces[0][i][j] << ' ';
+			for(int j=0;j<3;j++) os << faces[2][j][2-i] << ' ';
+			for(int j=2;j>=0;j--) os << faces[5][i][2-j] << ' ';
+			os << endl;
+		}
+		//Front
+		for(int i=0;i<3;i++){
+			os << ' ' << ' ' << ' ' << ' ' << ' ' << ' ';
+			for(int j=0;j<3;j++) os << faces[3][i][j] << ' ';
+			os << endl;
+		}
+	}
+}cube;
+
+int main()
+{
+	cube.input(cin);
+	cube.dump(cout);
+	return 0;
+}
